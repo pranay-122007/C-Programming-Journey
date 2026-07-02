a93af2619d2fcfe481e3120054174af2f7edffc5
Pranay Talware <pranaytalware7@gmail.com>
2026-07-02 23:41:37 +0530

    Dynamic memory allocation example with realloc
    Implement dynamic memory allocation using Realloc Function

---
+++ Lecture 18/Realloc_function.c
@@ -0,0 +1,37 @@
+//dynamic memory allocation
+//Realloc function
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	int n,*ptr,i;
+	printf("Enter the no of elements: ");
+	scanf("%d",&n);
+	ptr = calloc(n,sizeof(int));
+	if(ptr==NULL)
+	{
+		printf("Error in memory allocation!!!!");
+		exit(0); //for success = 1 and for failier = 0
+		//return
+	}
+	printf("Enter the elements: ");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",(ptr+i));
+	}
+	printf("Enter the new no of elements: ");
+	scanf("%d",&n);
+	int *ptr1;
+	ptr1=realloc(ptr,n*sizeof(int));
+	printf("Enter the new elements: ");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",(ptr1+i));
+	}
+	printf("Display the elements of array: \n");
+	for(i=0;i<n;i++)
+	{
+		printf("%d\t",*(ptr1+i));
+	}
+	free(ptr1);
+}
