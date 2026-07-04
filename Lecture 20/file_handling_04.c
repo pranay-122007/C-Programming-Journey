9f0a378c57b9b7a38f0fc8653bbf042930d9806a
Pranay Talware <pranaytalware7@gmail.com>
2026-07-04 21:34:06 +0530

    String Data write in created file using file handling

---
+++ Lecture 20/file_handling_04.c
@@ -0,0 +1,19 @@
+//file handling in c
+//file data read string
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	FILE *fp;
+	fp=fopen("NewFile.txt","r");
+	if(fp==NULL)
+	{
+		("File not exist!!!");
+		exit(0);
+	}
+	char ch[1000];
+	fgets(ch,1000,fp);
+	printf("%s",ch);
+	printf("\nData read Successfully!!!");
+	fclose(fp);
+}
